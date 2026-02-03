9956e668ca5600aabf805a3cfa72c576bd7fefe4
W-Leocadio <wesley.leocadio@luby.com.br>
2026-02-03 10:55:08 -0300

    Add quge5 ad script to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,12 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7010718300575269"
           crossOrigin="anonymous"
         ></script>
+        <script
+          src="https://quge5.com/88/tag.min.js"
+          data-zone="208166"
+          async
+          data-cfasync="false"
+        ></script>
       </head>
       <body className={`${inter.className} bg-slate-50 min-h-screen flex flex-col`}>
         <ServiceWorkerRegister />
